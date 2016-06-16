057ac78965f24595ef5a660672e4d2e0489c48a8
skamkar <sjk@zestfinance.com>
2016-06-15 22:01:45 -0700

    Handle basic product functional requirements

--- lib/product.rb
+++ lib/product.rb
@@ -1,17 +1,32 @@
 require 'byebug'
 
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
+
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock] ||= 0
     add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title(title)
+    @@products[@@products.find_index{ |product| product.title == title }]
+  end
+
+  def in_stock?
+    @stock > 0 ? true : false 
+  end
+
+  def self.in_stock
+    @@products.select{ |product| product.stock > 0}
   end
 
   private
