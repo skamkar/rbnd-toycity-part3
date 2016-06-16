66e9cb58d0f06194beb364052aa142dd21fbe91b
skamkar <sjk@zestfinance.com>
2016-06-15 21:37:23 -0700

    Prevent duplicate product creation

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,26 @@
+require 'byebug'
+
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    if @@products.map(&:title).include?(@title)
+    	raise DuplicateProductError, "#{@title} already exists."
+    else
+      @@products << self
+    end
   end
 end
